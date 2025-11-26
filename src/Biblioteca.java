355e833f61e2a4425dc1e006a95a36d87740e881
Arthur Vilasboas Marques <teuemaildopgithub@gmail.com>
2025-11-26 13:23:22 -0300

    Feita as Funções de Adicionar, Listar e Emprestar Livro

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -12,11 +12,22 @@ public class Biblioteca {
         Servicos servicos = new Servicos();
 
         boolean parar = false;
+        int id_livro = 1;
         do{
             int opcao = servicos.opcao();
 
             switch (opcao) {
                 case 1:
+                    livros.add(servicos.adicionarLivro(id_livro));
+                    id_livro++;
+                    break;
+                case 2:
+                    servicos.listarLivros(livros);
+                    break;
+                case 3:
+                    System.out.println("Digite o ID do Livro que deseja emprestar:");
+                    id_livro = scanner.nextInt();
+                    servicos.emprestarLivro(livros, id_livro);
                     break;
                 case 6:
                     parar = true;
--- src/Servicos.java
+++ src/Servicos.java
@@ -1,10 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Servicos   {
+public class Servicos {
 
     Scanner scanner = new Scanner(System.in);
 
-    public int opcao(){
+    public int opcao() {
         System.out.println("Digite a opção Desejada:");
         System.out.println("[1] Adicionar Livros");
         System.out.println("[2] Listar Livros");
@@ -16,4 +17,65 @@ public class Servicos   {
         scanner.nextLine();
         return opcao;
     }
+
+    // Função de Adicionar Livro
+    public Livro adicionarLivro(int id) {
+        System.out.println("Digite o Título do Livro:");
+        String titulo = scanner.nextLine();
+
+        System.out.println("Digite o Nome do Autor:");
+        String autor = scanner.nextLine();
+
+        System.out.println("Digite o Ano do Livro:");
+        int ano = scanner.nextInt();
+        scanner.nextLine();
+
+        Livro livro = new Livro(id, titulo, autor, ano);
+
+        System.out.println("Livro adicionado com Sucesso!");
+        System.out.println();
+
+        return livro;
+    }
+
+    // Função Listar Livros
+    public  void listarLivros(ArrayList<Livro> livros) {
+        if (livros.isEmpty()) {
+            System.out.println();
+            System.out.println("Nenhum Livro Cadastrado em nosso Sistema!");
+            System.out.println();
+            return;
+        }
+        System.out.println();
+        System.out.println("Lista de Livros: ");
+        for (int i = 0; i < livros.size(); i++) {
+            System.out.println(livros.get(i).toString());
+        }
+        System.out.println();
+    }
+
+    // Função Emprestar Livro
+    public void emprestarLivro(ArrayList<Livro> livros, int id) {
+        for (Livro livro : livros) {
+            if (livro.getId() == id) {
+                if (livro.getDisponibilidade()) {
+                    livro.setDisponibilidade(false);
+                    System.out.println();
+                    System.out.println("Livro emprestado com sucesso: " + livro.getTitulo());
+                    System.out.println();
+                } else {
+                    System.out.println();
+                    System.out.println("Este livro já está emprestado!");
+                    System.out.println();
+                }
+                return;
+            }
+        }
+
+
+        System.out.println();
+        System.out.println("Livro com ID " + id + " não encontrado!");
+        System.out.println();
+    }
+
 }
